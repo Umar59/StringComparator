af63c93a95bc76eb77879caf41ddfc864a8da111
Umar Bhat <u.bhat@icvr.io>
2022-06-16 23:41:29 +0300

    added the algorithms itself and 20% unit test coverage

---
+++ StringCompare/StringComparatorFull.cs
@@ -0,0 +1,328 @@
+﻿namespace StringCompare
+{
+    public struct ComparatorResults
+    {
+        public Dictionary<string, string> WrongRecognition { get; }
+        public Dictionary<string, int> RecognizedMultipleTimes { get; }
+        public List<string> NotRecognizedWords { get; }
+        public float Similarity { get; }
+
+        public Dictionary<string, string> Pairs { get; }
+
+        public ComparatorResults(Dictionary<string, string> pairs, Dictionary<string, string> wrongRecognition,
+            Dictionary<string, int> recognizedMultipleTimes, List<string> notRecognizedWords, float similarity)
+        {
+            Pairs = pairs;
+            WrongRecognition = wrongRecognition;
+            RecognizedMultipleTimes = recognizedMultipleTimes;
+            NotRecognizedWords = notRecognizedWords;
+            Similarity = similarity;
+        }
+    }
+
+    public class StringComparatorFull
+    {
+        public  ComparatorResults Compare(List<string> initial, List<string> voiceRec)
+        {
+            int initialCount = initial.Count;
+            int voiceRecCount = voiceRec.Count;
+
+            MakeLowerCase(initial);
+            MakeLowerCase(voiceRec);
+
+            var recognizedMultipleTimes = RemoveFirstAppearedDuplicates(voiceRec);
+
+            Dictionary<int, string> initialWithIndex = new Dictionary<int, string>();
+            Dictionary<int, string> voiceWithIndex = new Dictionary<int, string>();
+
+            CreateIndexedList(initial, initialWithIndex);
+            CreateIndexedList(voiceRec, voiceWithIndex);
+
+            Dictionary<int, string> initialWithIndexCopy = new Dictionary<int, string>(initialWithIndex);
+            Dictionary<int, string> voiceWithIndexCopy = new Dictionary<int, string>(voiceWithIndex);
+
+            Dictionary<int, int> pairsIndices = new Dictionary<int, int>();
+
+            CalculatePairs(pairsIndices, voiceWithIndex, initialWithIndex);
+
+            Dictionary<string, string> pairsValues = new Dictionary<string, string>();
+
+            foreach (var index in pairsIndices)
+            {
+                pairsValues.Add(initialWithIndexCopy[index.Key], voiceWithIndexCopy[index.Value]);
+            }
+
+            List<int> notRecognizedIndices = new List<int>();
+            Dictionary<string, string> wrongRecognition = new Dictionary<string, string>();
+
+            CalculateWrongAndNotRecognized(wrongRecognition, notRecognizedIndices, voiceWithIndexCopy,
+                initialWithIndexCopy,
+                initialWithIndex, voiceWithIndex, pairsIndices);
+
+            List<string> notRecognizedWords = IndicesToValues(notRecognizedIndices, initialWithIndexCopy);
+
+
+            var similarity = CalculateSimilarity(initialCount, voiceRecCount, pairsIndices.Count);
+
+            return new ComparatorResults(pairsValues, wrongRecognition, recognizedMultipleTimes, notRecognizedWords,
+                similarity);
+        }
+
+        private  List<T> IndicesToValues<T>(IEnumerable<int> indices, Dictionary<int, T> indexedValues)
+        {
+            List<T> values = new List<T>();
+
+            foreach (var index in indices)
+            {
+                if (indexedValues.ContainsKey(index))
+                {
+                    values.Add(indexedValues[index]);
+                }
+            }
+
+            return values;
+        }
+
+        public int Test(int a)
+        {
+            return a + 1;
+        }
+
+        private  void CreateIndexedList<T>(List<T> convertInit, Dictionary<int, T> converted)
+        {
+            for (var i = 0; i < convertInit.Count; i++)
+            {
+                converted.Add(i, convertInit[i]);
+            }
+        }
+
+        private  void CalculateWrongAndNotRecognized
+        (
+            Dictionary<string, string> wrongRecognition,
+            List<int> notRecognizedIndices,
+            Dictionary<int, string> voiceWithIndexCopy,
+            Dictionary<int, string> initialWithIndexCopy,
+            Dictionary<int, string> initialWithIndex,
+            Dictionary<int, string> voiceWithIndex,
+            Dictionary<int, int> pairs
+        )
+        {
+            //тут мы обязательно смотрим из исходных слов в выходные
+
+            Dictionary<List<int>, List<int>> groups = new Dictionary<List<int>, List<int>>();
+
+
+            foreach (var keyValuePair in pairs)
+            {
+                int keyIndex = keyValuePair.Key + 1, valueIndex = keyValuePair.Value + 1;
+
+                List<int> initGroupMember = new List<int>();
+                List<int> voiceGroupMember = new List<int>();
+
+                while (!pairs.ContainsValue(valueIndex) && voiceWithIndexCopy.ContainsKey(valueIndex))
+                {
+                    voiceGroupMember.Add(valueIndex);
+
+                    valueIndex++;
+                }
+                //  0    1    2    3    4    5    6    7    8
+                // "c", "x", "y", "z", "b", "a";
+                // "c", "x", "y", "z", "x", "x", "y", "a", "f"
+
+                //                      4    5    6    8
+                //                     "b"       
+                //                     "x", "x", "y", "f"     
+                
+                while (!pairs.ContainsKey(keyIndex) && initialWithIndexCopy.ContainsKey(keyIndex))
+                {
+                    initGroupMember.Add(keyIndex);
+
+                    keyIndex++;
+                }
+
+                //потому что последующие слова могут быть неправильно обработанным словом, которому мы уже нашли пару.
+
+                if (voiceGroupMember.Count > 0 && initGroupMember.Count == 0)
+                {
+                    initGroupMember.Add(keyValuePair.Key);
+                }
+
+                if (voiceGroupMember.Count > 0)
+                {
+                    groups.Add(initGroupMember, voiceGroupMember);
+                }
+                else if (initGroupMember.Count > 0)
+                {
+                    foreach (var s in initGroupMember)
+                    {
+                        notRecognizedIndices.Add(s);
+                    }
+                }
+            }
+
+            WrongRecognized(groups, voiceWithIndexCopy, initialWithIndexCopy, notRecognizedIndices, wrongRecognition);
+        }
+
+        private  void WrongRecognized
+        (
+            Dictionary<List<int>, List<int>> groups,
+            Dictionary<int, string> voiceWithIndexCopy,
+            Dictionary<int, string> initialWithIndexCopy,
+            List<int> notRecognizedIndices,
+            Dictionary<string, string> wrongRecognition)
+        {
+            foreach (var group in groups)
+            {
+                //короче тут надо описать матрицу сходимостей слов, а затем с большей сходимостью сгруппировать. Остальные добавить в NotRecognizedkekey
+
+                for (var i = 0; i < group.Value.Count; i++)
+                {
+                    float similarity = 0;
+                    int initPairIndex = 0;
+
+                    foreach (var initialRemainIndex in group.Key)
+                    {
+                        float currentSimilarity = StringSimilarity(voiceWithIndexCopy[group.Value[i]],
+                            initialWithIndexCopy[initialRemainIndex]);
+                        if (similarity < currentSimilarity)
+                        {
+                            similarity = currentSimilarity;
+                            initPairIndex = initialRemainIndex;
+                        }
+                    }
+
+                    if (similarity > 0)
+                    {
+                        if (wrongRecognition.ContainsKey(initialWithIndexCopy[initPairIndex]))
+                        {
+                            wrongRecognition[initialWithIndexCopy[initPairIndex]] += voiceWithIndexCopy[group.Value[i]];
+                        }
+                        else
+                        {
+                            wrongRecognition.Add(initialWithIndexCopy[initPairIndex],
+                                voiceWithIndexCopy[group.Value[i]]);
+                        }
+
+                        group.Value.Remove(group.Value[i]);
+                        i--;
+                    }
+                    else
+                    {
+                        //хз куда их посылать
+                    }
+                }
+
+                for (var i = 0; i < group.Key.Count; i++)
+                {
+                    if (!wrongRecognition.ContainsKey(initialWithIndexCopy[group.Key[i]]))
+                    {
+                        notRecognizedIndices.Add(group.Key[i]);
+                        group.Key.RemoveAt(i);
+                    }
+                }
+            }
+        }
+
+        private  float StringSimilarity(string a, string b)
+        {
+            return 0.8f;
+        }
+
+        private  void CalculatePairs(Dictionary<int, int> pairs,
+            Dictionary<int, string> voiceWithIndex,
+            Dictionary<int, string> initialWithIndex)
+        {
+            for (int i = 0; i <= voiceWithIndex.Last().Key; i++)
+            {
+                for (int j = initialWithIndex.First().Key; j <= initialWithIndex.Last().Key; j++)
+                {
+                    if (voiceWithIndex.ContainsKey(i) && initialWithIndex.ContainsKey(j))
+                    {
+                        if (voiceWithIndex[i] == initialWithIndex[j])
+                        {
+                            pairs.Add(j, i);
+                            voiceWithIndex.Remove(i);
+                            initialWithIndex.Remove(j);
+
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        private  void MakeLowerCase(List<string> initial)
+        {
+            for (int i = 0; i < initial.Count; i++)
+            {
+                initial[i] = initial[i].ToLower();
+            }
+        }
+
+        public Dictionary<string, int> RemoveFirstAppearedDuplicates(List<string> voiceRec)
+        {
+            Dictionary<string, int> duplicatesFromVoiceRec = new Dictionary<string, int>();
+            List<string> alreadyChecked = new List<string>();
+
+            for (int i = 0; i < voiceRec.Count; i++)
+            {
+                if (!alreadyChecked.Contains(voiceRec[i]))
+                {
+                    int appearancesCount = 1;
+
+                    for (int j = i + 1; j < voiceRec.Count; j++)
+                    {
+                        if (voiceRec[i] == voiceRec[j])
+                        {
+                            voiceRec.RemoveAt(j);
+                            appearancesCount++;
+
+                            alreadyChecked.Add(voiceRec[i]);
+                        }
+                        else
+                        {
+                            break;
+                        }
+                    }
+
+                    if (!alreadyChecked.Contains(voiceRec[i]))
+                    {
+                        alreadyChecked.Add(voiceRec[i]);
+                    }
+
+                    if (appearancesCount > 1)
+                    {
+                        duplicatesFromVoiceRec.Add(voiceRec[i], appearancesCount);
+                    }
+                }
+            }
+
+            return duplicatesFromVoiceRec;
+        }
+
+        public float CalculateSimilarity(float initialCount, float voiceRecCount, float similarPairsCount)
+        {
+            if (voiceRecCount <= 0 || initialCount <= 0 || similarPairsCount < 0)
+            {
+                return -1;
+            }
+            if (initialCount > voiceRecCount)
+            {
+                if (initialCount < similarPairsCount)
+                {
+                    return -1;
+                }
+                return similarPairsCount / initialCount;
+            }
+            else
+            {
+                if (voiceRecCount < similarPairsCount)
+                {
+                    return -1;
+                }
+                return similarPairsCount / voiceRecCount;
+            }
+
+        }
+    }
+}
---
+++ Tests/StringCompareTests.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.VisualStudio.TestPlatform.Utilities;
+using NUnit.Framework;
+using Moq;
+
+namespace Tests
+{
+    public class StringCompareTests
+    {
+        private StringCompare.StringComparatorFull _comparatorFull;
+
+        [SetUp]
+        public void Setup()
+        {
+            _comparatorFull = new StringCompare.StringComparatorFull();
+        }
+
+        [Test]
+        #region TestCases
+        [TestCase(3, 4, 2, ExpectedResult = 0.5f)]
+        [TestCase(4, 3, 2, ExpectedResult = 0.5f)]
+        [TestCase(10, 10, 9, ExpectedResult = 0.9f)]
+        [TestCase(10, 10, 10, ExpectedResult = 1f)]
+        [TestCase(30, 20, 0, ExpectedResult = 0f)]
+        [TestCase(float.MaxValue, float.MaxValue, float.MaxValue, ExpectedResult = 1f)]
+        [TestCase(float.MinValue, float.MinValue, float.MinValue, ExpectedResult = -1f)]
+        [TestCase(0, 0, 0, ExpectedResult = -1f)]
+        [TestCase(3, 4, 5, ExpectedResult = -1f)]
+        [TestCase(5, 4, 50, ExpectedResult = -1f)]
+        [TestCase(-1, -1, 1, ExpectedResult = -1f)]
+        [TestCase(1, -1, 1, ExpectedResult = -1f)]
+        [TestCase(-1, 1, 1, ExpectedResult = -1f)]
+        [TestCase(-1, -1, -1, ExpectedResult = -1f)]
+        [TestCase(1, 1, -1, ExpectedResult = -1f)]
+        [TestCase(1, -1, -1, ExpectedResult = -1f)]
+        [TestCase(-1, 1, -1, ExpectedResult = -1f)]
+        #endregion
+        
+        public float IsCalculatingWordsListSimilarity(float initialCountTest, float voiceRecCountTest, float similarPairsCountTest)
+        {
+            var similarity =
+                _comparatorFull.CalculateSimilarity(initialCountTest, voiceRecCountTest, similarPairsCountTest);
+            
+            return similarity;
+        }
+
+        #region TestCasesStruct
+
+        public struct RemovingList
+        {
+            public RemovingList(List<string> testCase, List<string> removedDuplicatesExpected, Dictionary<string, int> outPutExpected)
+            {
+                TestCase = testCase;
+                RemovedDuplicatesExpected = removedDuplicatesExpected;
+                OutPutExpected = outPutExpected;
+            }
+
+            public List<string> TestCase { get; }
+            public List<string> RemovedDuplicatesExpected { get; }
+            public Dictionary<string, int> OutPutExpected { get; }
+        }
+        #endregion testCasesStruct
+        
+        [Test]
+        public void IsRemovingFirstAppearedDuplicates()
+        {
+            List<string> beforeRemovingDuplicates;
+            List<string> afterRemovingDuplicates;
+            Dictionary<int, int> outputDuplicatesExpected;
+            int[][] d = new int[2][];
+            outputDuplicatesExpected = d.ToDictionary(ints => {  })
+            #region TestCases
+
+            List<RemovingList> TestCases = new List<RemovingList>()
+            {
+                new RemovingList(new List<string>()
+                {
+                    "c", "x", "x", "y", "z", "x", "x", "y", "a", "f"
+                }, new List<string>()
+                {
+                    "c", "x", "y", "z", "x", "x", "y", "a", "f"
+                }, new Dictionary<string, int>()
+                {
+                    {"x", 2}
+                }),
+                // new RemovingList(new List<string>()
+                // {
+                //     "c", "c", "c", "c", "c", "c", "c", "c", "c", "c"
+                // }, new List<string>()
+                // {
+                //     "c"
+                // }, new Dictionary<string, int>()
+                // {
+                //     {"c", 10}
+                // }),
+                new RemovingList(new List<string>()
+                {
+                    "c", "c", "c", "y", "c", "y", "y", "y", "f", "f"
+                }, new List<string>()
+                {
+                    "c", "y", "c", "y", "y", "y", "f"
+                }, new Dictionary<string, int>()
+                {
+                    {"c", 3},
+                    {"f", 2}
+                }),
+                // new RemovingList(new List<string>()
+                // {
+                //     "c", "y", "c", "y", "y", "y", "f"
+                // }, new List<string>()
+                // {
+                //     "c", "y", "c", "y", "y", "y", "f"
+                // }, new Dictionary<string, int>()
+                // {
+                // }),
+                // new RemovingList(new List<string>()
+                // {
+                // }, new List<string>()
+                // {
+                // }, new Dictionary<string, int>()
+                // {
+                // }),
+            };
+
+            #endregion
+
+            foreach (var removingList in TestCases)
+            {
+                List<string> input = new List<string>(removingList.TestCase);
+
+                Dictionary<string, int> outputActual = _comparatorFull.RemoveFirstAppearedDuplicates(input);
+            
+                Assert.AreEqual(removingList.RemovedDuplicatesExpected, input);
+               // Assert.AreEqual(removingList.OutPutExpected, outputActual);
+            }
+        }
+    }
+}
